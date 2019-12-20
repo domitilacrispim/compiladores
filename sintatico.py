e8325f026e0b4531f30acc80b1abb959372ef62f
domitilacrispim <domitilacrispim@gmail.com>
2019-12-20 11:26:59 -0300

    Add files via upload

--- sintatico.py
+++ sintatico.py
@@ -1,11 +1,15 @@
 import csv
 import sys
+tabela_simbol=[]
+col=0
+lin=0
 def lexico(pos):
- 
+ global lin, col, tabela_simbol
  comp = open('compiladores.csv', 'r')
  dici = {}
  reader = csv.reader(comp)
  flag = 0
+ f_inicio = 0
  # carrega informações da tabela de produção
  cabecalho=[]
  for linha in reader:
@@ -19,21 +23,27 @@ def lexico(pos):
    flag=flag+1 
  #print(dici["e"][0])
  arq = open("programa", "r")
- col=0
- lin=0
  estado=0
  arquivo=arq.read()
+ inicial=0
  atual=arquivo[pos]
  while(True):
+   if(atual=="\n"):
+    lin=lin+1
+    col=0
    if(atual==" " or atual=="\n" or atual=="EOF"):
     atual="branco"
+   elif(f_inicio==0):
+    f_inicio=1
+    inicial=pos
   # print(estado, atual)
    try:
     estado=int(dici[atual][estado])
    except:
-    print("O simbolo nao faz parte da gramatica")
-    return "$", -1
+    print("O simbolo nao faz parte da gramatica linha:", lin, "coluna:", col )
+    return "$", -1, -1
    pos=pos+1
+   col=col+1
    if(estado==-1):
     print("Error na posicao", pos)
     sys.exit(0)
@@ -41,40 +51,51 @@ def lexico(pos):
     if(dici["value"][estado]==">" or dici["value"][estado]==">=" or dici["value"][estado]=="<" or dici["value"][estado]=="<>" or dici["value"][estado]=="==" or dici["value"][estado]=="<="):
      if(int(dici["goBack"][estado])==1):
        pos=pos-1
-     return "relacional",pos
+       col=col-1
+     return "relacional",pos, dici["value"][estado]
     elif(dici["value"][estado]=="+"  or dici["value"][estado]=="/" or dici["value"][estado]=="*" or dici["value"][estado]=="-"):
      if(int(dici["goBack"][estado])==1):
       pos=pos-1
-     return "aritmetico",pos
+      col=col-1
+     return "aritmetico",pos, dici["value"][estado]
     elif(dici["value"][estado]=="="):  
      if(int(dici["goBack"][estado])==1):
       pos=pos-1
-     return "atribuicao",pos
+      col=col-1
+     return "atribuicao",pos, dici["value"][estado]
+    elif(dici["value"][estado]=="id"):  
+     if(int(dici["goBack"][estado])==1):
+      pos=pos-1
+      col=col-1
+     if(arquivo[inicial:pos] not in tabela_simbol):
+      tabela_simbol.append(arquivo[inicial:pos])
+      print(arquivo[inicial:pos],  inicial,pos+1)
+     return "id",pos, arquivo[inicial:pos]
     else:
       if(int(dici["goBack"][estado])==1):
        pos=pos-1
-      return dici["value"][estado], pos
-    if(int(dici["goBack"][estado])==1):
-     pos=pos-1
-    estado=0
+       col=col-1
+      return dici["value"][estado], pos ,dici["value"][estado]
    if(pos==len(arquivo)):
     break  
    atual=arquivo[pos]
  atual="branco"
  estado=int(dici[atual][estado])
  pos=pos+1
+ col=col-1
  if(estado==-1):
     print("Error na posicao", pos)
  if(int(dici["isFinal"][estado])==1):
     print(dici["value"][estado])
- return "$",-1
+ return "$",-1, -1
  
 
 class No:
-    def __init__(self, valor):
+    def __init__(self, valor, n):
         self.valor = valor
         self.filhos = []
         self.processado = False
+        self.nivel=n
 
     def processador(self, valor):
         if(not(self.processado) and self.valor == valor):
@@ -85,7 +106,7 @@ class No:
 
     def addFilho(self, valor, pai):
         if(pai == self.valor and not(self.processado)):
-            self.filhos.append(No(valor))
+            self.filhos.append(No(valor, self.nivel+1))
             return 1
         if(len(self.filhos) == 0):
             return 0
@@ -93,20 +114,29 @@ class No:
             if(filho.addFilho(valor, pai) == 1):
                 return 1
 
-    def percorre(self):
+    def percorre(self, n):
         flagui = 0
-        if(len(self.filhos) != 0):
-            print("")
-        for filho in self.filhos:
-            if(flagui == 0):
-                print("<", end="")
-                flagui = 1
-            print(filho.valor, " ", end="")
-        if(flagui == 1):
-            print(">", end="")
-        for filho in self.filhos:
-            filho.percorre()
-
+        if(self.nivel>n):
+         return 0
+        if(self.nivel==n):
+         for filho in reversed(self.filhos):
+             if(flagui == 0):
+                 print("<", end="")
+                 flagui = 1
+             print(filho.valor, " ", filho.nivel, " " , end="")
+         if(flagui == 1):
+             print(">")
+         for filho in reversed(self.filhos):
+             if(filho.percorre(n+1)==3):
+              flagui=3
+         return 3
+        if(self.nivel<n):
+         for filho in reversed(self.filhos):
+             if(filho.percorre(n+1)==3):
+              flagui=3
+         if(flagui==3):
+     	    print("")
+         
 
 prod = open('producoes.csv', 'r')
 n_term = open('n_terminais.csv', 'r')
@@ -148,22 +178,22 @@ for i in reader:
 # lexema = input().split()  # ler codigo
 
 pilha = ["S"]
-raiz = No("S")
+raiz = No("S",0)
 flag = 0
 flag_proc = 0
-prox, pos = lexico(0)
+prox, pos, valorz = lexico(0)
 print(prox)
 # print(prox)
 while(len(pilha) > 0):
     flag_proc = 0
     x = pilha[len(pilha)-1]
-    print(pilha)
+#    print(pilha)
     # print(prox)
     if(x not in nao_t):
         if(x == prox):
             pilha.pop(len(pilha)-1)
             # flag = flag+1
-            prox, pos = lexico(pos)  # chamar lexico
+            prox, pos, valorz = lexico(pos)  # chamar lexico
           #  print(prox)
             if(prox=="$"):
            #  print(prox)
@@ -193,6 +223,8 @@ if (prox != "$" or len(pilha)!=0):
 else:
     print("\nCadeia Aceita!!")
     print('\nArvore:')
-    print(raiz.valor, end="")
-    raiz.percorre()
+    print(raiz.valor)
+    raiz.percorre(0)
+global tabela_simbol
+print(tabela_simbol)
 
